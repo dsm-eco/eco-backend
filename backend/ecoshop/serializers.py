e0421b7b20a2c163428ddf060de6936551ff3daa
hanna <namnam1178@daum.net>
2021-04-04 22:42:40 +0900

    [ADD] Create shop serializer

--- backend/ecoshop/serializers.py
+++ backend/ecoshop/serializers.py
@@ -1,10 +1,32 @@
 from rest_framework import serializers
 
-from ecoshop.models import Shop
+from ecoshop.models import Shop, ShopImage
+
+
+class ShopImageSerializer(serializers.ModelSerializer):
+
+    image = serializers.ImageField(use_url=True)
+
+    class Meta:
+        model = ShopImage
+        fields = ['image']
 
 
 class ShopSerializer(serializers.ModelSerializer):
+    images = serializers.SerializerMethodField()
+
+    def get_images(self, obj):
+
+        image = obj.diaryimage_set.all()
+        return ShopImageSerializer(instance=image, many=True, context=self.context).data
 
     class Meta:
         model = Shop
         fields = ('id', 'name', 'address', 'content', 'heart_cnt', 'heart')
+
+    def create(self, validated_data):
+        instance = Shop.objects.create(**validated_data)
+        image_set = self.context['request'].FILES
+        for image_data in image_set.getlist('image'):
+            ShopImage.objects.create(diary=instance, image=image_data)
+        return instance
