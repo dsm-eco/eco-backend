6d0a5d52dcd224aaad04719030d4c2f0c437d4ca
hanna <namnam1178@daum.net>
2021-04-09 14:38:07 +0900

    [ADD] Create post viewset

--- backend/event/models.py
+++ backend/event/models.py
@@ -5,6 +5,7 @@ from user_system.models import User
 
 class Event(models.Model):
     user = models.ForeignKey(User, related_name="event", on_delete=models.CASCADE, null=True)
+    nickname = models.CharField(max_length=45, null=True, blank=True)
     content = models.CharField(max_length=200, blank=True, null=True)
     heart_cnt = models.IntegerField(default=0, blank=True, null=False)
     heart = models.BooleanField(default=False, blank=True)
--- backend/event/views.py
+++ backend/event/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from event.models import Event
+from event.serializers import EventSerializer
+
+
+class EventViewSet(viewsets.ModelViewSet):
+    serializer_class = EventSerializer
+    permission_classes = [IsAuthenticated, ]
+    queryset = Event.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user, nickname=self.request.user.nickname)
+
+    @action(detail=True, methods=['get'])
+    def heart(self, request):
+        instance = self.get_object()
+        instance.heart = not instance.heart
+
+        if not instance.heart:
+            instance.heart_cnt -= 1
+        else:
+            instance.heart_cnt += 1
+
+        instance.save()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
