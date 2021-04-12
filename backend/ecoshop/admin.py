ebcc931b1c9fdb15f588d9a082493f593bc6c968
hanna <namnam1178@daum.net>
2021-04-12 20:08:02 +0900

    [ADD] Add admin page - shop post

--- backend/ecoshop/admin.py
+++ backend/ecoshop/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 from ecoshop.models import ShopPost
 
 
-admin.site.register(ShopPost)
+@admin.register(ShopPost)
+class EcoShopPostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nickname', 'name', 'content', 'heart_cnt', 'report']
