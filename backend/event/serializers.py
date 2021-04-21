ced87784b0249cdfc4bf38549229f9119a427e99
hanna <namnam1178@daum.net>
2021-04-21 14:59:54 +0900

    [ADD] Create eco shop heart api

--- backend/event/serializers.py
+++ backend/event/serializers.py
@@ -13,10 +13,22 @@ class EventImageSerializer(serializers.ModelSerializer):
 
 class EventSerializer(serializers.ModelSerializer):
     images = serializers.SerializerMethodField()
+    heart_cnt = serializers.SerializerMethodField()
+    heart = serializers.SerializerMethodField()
 
     def get_images(self, obj):
         image = obj.eventimage_set.all()
         return EventImageSerializer(instance=image, many=True, context=self.context).data
+
+    def get_heart_cnt(self, obj):
+        return obj.count_likes_user()
+
+    def get_heart(self, obj):
+        user = self.context.get('request').user
+        if obj.event_likes_user.filter(id=user.id).exists():
+            return True
+        else:
+            return False
 
     class Meta:
         model = Event
--- backend/event/views.py
+++ backend/event/views.py
@@ -1,3 +1,6 @@
+import json
+
+from django.http import HttpResponse
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
@@ -14,6 +17,11 @@ class EventViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user, nickname=self.request.user.nickname)
+        
+    def get_serializer_context(self):
+        context = super(EventViewSet, self).get_serializer_context()
+        context.update({"request": self.request})
+        return context
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -22,3 +30,19 @@ class EventViewSet(viewsets.ModelViewSet):
             return Response(status=status.HTTP_204_NO_CONTENT)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=['GET'])
+    def heart(self, requset, pk):
+        instance = self.get_object()
+        user = requset.user
+
+        if instance.event_likes_user.filter(id=user.id).exists():
+            instance.event_likes_user.remove(user)
+            message = 'false'
+        else:
+            instance.event_likes_user.add(user)
+            message = 'true'
+
+        context = {'heart_cnt': instance.count_likes_user(), 'heart': message}
+        return HttpResponse(json.dumps(context), content_type="application/json")
+
