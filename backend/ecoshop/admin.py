b6d41c8cacaffb0c4f5a1094ae0a484cd9ea8ba3
hanna <namnam1178@daum.net>
2021-04-22 11:59:41 +0900

    [FIX] Fix admin page - add heart_cnt field

--- backend/ecoshop/admin.py
+++ backend/ecoshop/admin.py
@@ -5,9 +5,12 @@ from ecoshop.models import ShopPost
 
 @admin.register(ShopPost)
 class EcoShopPostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'nickname', 'name', 'report', 'permission']
-    fields = ['nickname', 'name', 'address', 'content', 'report']
+    list_display = ['id', 'nickname', 'name', 'report', 'permission', 'heart_cnt']
+    fields = ['nickname', 'name', 'address', 'content', 'report', 'heart_cnt']
     readonly_fields = ['nickname', 'name', 'address', 'content', 'report']
     list_editable = ('permission', )
     list_filter = ('permission', )
     ordering = ('-report', 'id')
+
+    def heart_cnt(self, obj):
+        return obj.shop_post_likes_user.count()
