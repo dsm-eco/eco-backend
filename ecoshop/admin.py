59f34a61f3dee065b1c154a15ebff1209a119a1e
hanna <namnam1178@daum.net>
2021-06-01 14:46:42 +0900

    [ADD] Add shop image model in admin

--- ecoshop/admin.py
+++ ecoshop/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from ecoshop.models import Shop
+from ecoshop.models import Shop, ShopImage
 
 
 @admin.register(Shop)
 class EcoShopAdmin(admin.ModelAdmin):
     list_display = ['id', 'name']
     fields = ['name', 'address', 'content']
+
+
+@admin.register(ShopImage)
+class ShopImageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'shop_id']
+    fields = ['shop_id', 'image']
